deaa64baa65611a949a5971b6437a404e2b92137
GallaKiranmai <gallakiranmai@gmail.com>
2026-01-21 20:08:19 +0530

    Update Topbar.jsx

--- frontend/Topbar.jsx
+++ frontend/Topbar.jsx
@@ -1,11 +1,8 @@
-import logo from "../assets/CC6.png";
-
 export default function Topbar() {
   return (
     <div className="topbar">
-      <div className="topbar-left">
-        <img src={logo} alt="Campus Mate Logo" className="topbar-logo" />
-        <h1>CAMPUS MATE</h1>
+      <div className="topbar-center">
+        <h1 className="brand">CAMPUS MATE</h1>
       </div>
     </div>
   );
